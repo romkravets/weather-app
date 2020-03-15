4a1f80270016c771db5ca93bf00eb6af2ff27cbe
romkravets <romann.kravets@gmail.com>
2020-03-15 20:43:27 +0200

    Added jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 
-import 'bootstrap/dist/bootstrap.min.css';
+import 'weather-icons/css/weather-icons.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Weather from './app_component/weather.component';
 
 function App() {
--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -5,8 +5,25 @@ const Weather = () => {
       <div className="container">
          <div className="cards">
             <h1>London</h1>
-            </div>
+            <h5 className="py-4">
+               <i className="wi wi-day-sunny display-1"></i>
+            </h5>
+            <h2 className="py-2">25&deg;</h2>
+
+            {minmaxTemp(24, 19)}
+
+            <h3>Slow Rein</h3>
+         </div>
       </div>
+   );
+}
+
+function minmaxTemp(min,max) {
+   return (
+      <h3>
+         <span className="px-4">{min}&deg;</span>
+         <span className="px-4">{max}&deg;</span>
+      </h3>
    );
 }
 
